c9b7ffe1731994d0c65bf68e131e4597ea74a3d4
Gavin <gavinjboyd@gmail.com>
2021-02-14 18:49:08 +0900

    add tests for user app

--- Readme.md
+++ Readme.md
@@ -4,6 +4,16 @@
 
 docker-compose up
 
+## Django commands
+
+docker-compose run app sh -c "python manage.py createsuperuser"
+
+###Testing credentials
+test@email.com
+
+password
+
 ##Testing
 
-docker-compose run app sh -docker-compose run app sh -c "python manage.py test && flake8"
+[comment]: <> (--rm removes after running)
+docker-compose run --rm app sh -docker-compose run app sh -c "python manage.py test && flake8"
---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,56 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+class PublicUserAPITests(TestCase):
+    """Test the users api public"""
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_create_user_valid_success(self):
+        """Test creating user with valid payload success"""
+        payload = {
+            "email": "test@email.com",
+            "password": "Password2132",
+            "name": "test"
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        # unwind response with **
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
+
+    def test_user_exists(self):
+        """Tests creating user with same credentials should fail"""
+        payload = {
+            "email": "test@email.com",
+            "password": "Password2132"
+        }
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Password must be more then 5 characters"""
+        payload = {
+            "email": "test2@email.com",
+            "password": " pass1"
+        }
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
