d8f5f7faaa71b6652f80b31e9b02c17b09a70540
Gavin <gavinjboyd@gmail.com>
2021-03-06 13:25:15 +0900

    feature: added auth token serializer and view

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,15 @@
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create user"""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
