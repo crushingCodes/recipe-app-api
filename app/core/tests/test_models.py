88040a612ff12f9d649e7cef285f82c1ddd625cf
Gavin <gavinjboyd@gmail.com>
2021-03-07 12:15:32 +0900

    add Tag model and test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,15 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core.models import Tag
+
+
+def sample_user(email='test@email.com', name='test', password='Password1'):
+    """Create sample user"""
+    return get_user_model().objects.create_user(
+        email=email,
+        name=name,
+        password=password
+    )
 
 
 class ModelTests(TestCase):
@@ -35,3 +45,12 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test tag string representation"""
+        tag = Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
