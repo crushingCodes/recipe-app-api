c6c6a671bb85aecc943776fe08086bff0d1667e0
Gavin <gavinjboyd@gmail.com>
2021-03-07 15:57:45 +0900

    added create tag endpoint

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -13,11 +13,14 @@ TAG_URL = reverse('recipe:tag-list')
 
 def sample_user(email='test@email.com', name='test', password='Password1'):
     """Create sample user"""
-    return get_user_model().objects.create_user(
-        email=email,
-        name=name,
-        password=password
-    )
+    try:
+        return get_user_model().objects.get(email=email)
+    except Exception:
+        return get_user_model().objects.create_user(
+            email=email,
+            name=name,
+            password=password
+        )
 
 
 class PublicTagsAPITest(TestCase):
@@ -61,3 +64,26 @@ class PrivateAPITagsTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        """Test creating a tag works"""
+        payload = {
+            'name': "Entree",
+            'user': sample_user()
+        }
+        res = self.client.post(TAG_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        tag_exists = Tag.objects.filter(name=payload['name']).exists()
+        self.assertTrue(tag_exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a tag with invalid name fails"""
+        payload = {
+            'name': '',
+            'user': sample_user()
+        }
+        res = self.client.post(TAG_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        tag_exists = Tag.objects.filter(name='')
+        self.assertFalse(tag_exists)
