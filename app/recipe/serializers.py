4b0e585d2239c51901e857eab0db07729cc8d01e
Gavin <gavinjboyd@gmail.com>
2021-03-12 23:18:09 +0900

    test: recipe detail endpoint

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -29,3 +29,6 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes', 'price', 'link',)
         read_only_fields = ('id',)
+
+
+# class RecipeDetailSerializer(serializers.Mo)
